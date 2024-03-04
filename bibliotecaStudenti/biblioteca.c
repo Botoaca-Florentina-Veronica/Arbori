aa6ec775d92e4babacbe8b3364694e80717385c7
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-03-04 15:23:34 +0200

    Update biblioteca.c

--- bibliotecaStudenti/biblioteca.c
+++ bibliotecaStudenti/biblioteca.c
@@ -11,141 +11,201 @@ organizate sub forma unui arbore binar ordonat, care se foloseste si de functiil
 #include <string.h>
 #include "biblioteca.h"
 
-// Funcție pentru alocarea memoriei pentru un nou nod Student
-Student *createNode(char nume[], int nota, int varsta) 
+
+Arbore initializareArbore() 
 {
-    Student *newNode;
-    newNode = (Student *)malloc(sizeof(Student));
+    return NULL;
+}
+
+// Funcție pentru alocarea memoriei pentru un nou nod Student
+node *createNode(Student student) 
+{
+    node *newNode;
+    newNode = (node *)malloc(sizeof(node));
     if (newNode == NULL) 
     {
         printf("Eroare la alocarea memoriei pentru un nou nod.\n");
         exit(1);
     }
-    strcpy(newNode->nume, nume);
-    newNode->nota = nota;
-    newNode->varsta = varsta;
+    newNode->student = student;
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
 }
 
-// Funcție pentru adăugarea unui nod în arbore
-Student *insertNode(Student *radacina, char nume[], int nota, int varsta) 
+// Funcție pentru adăugarea unui nod în arbore(vom insera alfabetic)
+void addNode(Arbore *radacina, Student student) 
 {
-    if (radacina == NULL) 
+    if (*radacina == NULL) 
     {
         //daca rădăcina nu există, atunci putem adăuga noul nod, el devenind noua rădăcină
-        return createNode(nume, nota, varsta);
+        *radacina = createNode(student);
     }
-    if (strcmp(nume, radacina->nume) < 0) 
+    else 
     {
-        radacina->left = insertNode(radacina->left, nume, nota, varsta);
-    } 
-    else if (strcmp(nume, radacina->nume) > 0) 
-    {
-        radacina->right = insertNode(radacina->right, nume, nota, varsta);
+        if (strcmp(student.nume, (*radacina)->student.nume) < 0) 
+        {
+            addNode(&(*radacina)->left, student);
+        } 
+        else 
+        {
+            addNode(&(*radacina)->right, student);
+        }
     }
-    return radacina;
 }
 
-// Funcție pentru găsirea celui mai mic nod în arbore (utilizată pentru ștergerea unui nod)
-Student *findMinNode(Student* node) 
+
+node *findMin(node *nod) 
 {
-    Student* current = node;
-    while (current && current->left != NULL) 
+    while (nod->left != NULL) 
     {
-        current = current->left;
+        nod = nod->left;
     }
-    return current;
+    return nod;
 }
 
-// Funcție pentru ștergerea unui nod din arbore
-Student *deleteNode(Student *radacina, char nume[])
- {
+node *deleteNod(node *radacina, char *nume) 
+{
     if (radacina == NULL) 
     {
         return radacina;
     }
-    if (strcmp(nume, radacina->nume) < 0) 
+
+    // Găsește nodul de șters în subarborele stâng sau drept
+    if (strcmp(nume, radacina->student.nume) < 0) 
     {
-        radacina->left = deleteNode(radacina->left, nume);
+        radacina->left = deleteNod(radacina->left, nume);
     } 
-    else if (strcmp(nume, radacina->nume) > 0) 
+    else if (strcmp(nume, radacina->student.nume) > 0) 
     {
-        radacina->right = deleteNode(radacina->right, nume);
+        radacina->right = deleteNod(radacina->right, nume);
     } 
     else 
     {
+        // Nodul de șters are 0 sau 1 copil
         if (radacina->left == NULL) 
         {
-            Student* temp = radacina->right;
-            free(radacina);
-            return temp;
-        } 
-        else if (radacina->right == NULL) 
-        {
-            Student* temp = radacina->left;
+            node *temp = radacina->right;
             free(radacina);
             return temp;
         }
-        Student* temp = findMinNode(radacina->right);
-        strcpy(radacina->nume, temp->nume);
-        radacina->nota = temp->nota;
-        radacina->varsta = temp->varsta;
-        radacina->right = deleteNode(radacina->right, temp->nume);
+        else if (radacina->right == NULL) 
+        {
+            node *temp = radacina->left;
+            free(radacina);
+            return temp;
+        }
+
+        // Nodul de șters are 2 copii
+        node *temp = findMin(radacina->right);
+        radacina->student = temp->student;
+        radacina->right = deleteNod(radacina->right, temp->student.nume);
     }
     return radacina;
 }
 
-// Funcție pentru căutarea unui nod în arbore
-Student* searchNode(Student *radacina, char nume[]) 
-{
-    if (radacina == NULL || strcmp(nume, radacina->nume) == 0) 
-    {
-        return radacina;
-    }
-    if (strcmp(nume, radacina->nume) < 0) 
-    {
-        return searchNode(radacina->left, nume);
-    }
-    return searchNode(radacina->right, nume);
-}
 
-// Funcție pentru afișarea arborelui în preordine (radacina, stanga, dreapta)
-void preOrderTraversal(Student* radacina)
+// Funcție pentru afișarea arborelui în inordine (stanga, radacina, dreapta)
+void inOrderTraversal(Arbore radacina) 
 {
     if (radacina != NULL) 
     {
-        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->nume, radacina->nota, radacina->varsta);
+        inOrderTraversal(radacina->left);
+        printf("Nume: %s, Varsta: %d, Nota: %.2f\n", radacina->student.nume, radacina->student.varsta, radacina->student.nota);
+        inOrderTraversal(radacina->right);
+    }
+}
+
+
+// Funcție pentru afișarea arborelui în preordine (radacina, stanga, dreapta)
+void preOrderTraversal(Arbore radacina)
+{
+    if (radacina != NULL) 
+    {
+        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->student.nume, radacina->student.nota, radacina->student.varsta);
         preOrderTraversal(radacina->left);
         preOrderTraversal(radacina->right);
     }
 }
 
-// Funcție pentru afișarea arborelui în inordine (stanga, radacina, dreapta)
-void inOrderTraversal(Student *radacina) 
-{
-    if (radacina != NULL) 
-    {
-        inOrderTraversal(radacina->left);
-        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->nume, radacina->nota, radacina->varsta);
-        inOrderTraversal(radacina->right);
-    }
-}
 
 // Funcție pentru afișarea arborelui în postordine (stanga, dreapta, radacina)
-void postOrderTraversal(Student *radacina) 
+void postOrderTraversal(Arbore radacina) 
 {
     if (radacina != NULL) 
     {
         postOrderTraversal(radacina->left);
         postOrderTraversal(radacina->right);
-        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->nume, radacina->nota, radacina->varsta);
+        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->student.nume, radacina->student.nota, radacina->student.varsta);
     }
 }
 
+// Funcție pentru citirea datelor dintr-un fișier binar într-un tablou de structuri
+void readingFromBinaryFile(const char *fileName, Student *studenti, int nrInregistrari) 
+{
+    FILE *file = fopen(fileName, "rb");
+    if (file == NULL) 
+    {
+        printf("Eroare la deschiderea fisierului %s\n", fileName);
+        exit(EXIT_FAILURE);
+    }
+
+    // Citirea datelor pentru fiecare înregistrare
+    fread(studenti, sizeof(Student), nrInregistrari, file);
+    fclose(file);
+}
+
+// Funcție pentru citirea unei anumite înregistrări identificate prin numărul său din fișier
+void readRegistration(const char *fileName, Student *student, int nrInregistrare) 
+{
+    FILE *file = fopen(fileName, "rb");
+    if (file == NULL) 
+    {
+        printf("Eroare la deschiderea fisierului %s\n", fileName);
+        exit(EXIT_FAILURE);
+    }
+
+    // Folosirea fseek pentru a poziționa cursorul la începutul înregistrării dorite
+    fseek(file, (nrInregistrare - 1) * sizeof(Student), SEEK_SET);
+
+    // Citirea înregistrării
+    fread(student, sizeof(Student), 1, file);
+    fclose(file);
+}
+
+// Funcție pentru scrierea datelor dintr-un arbore într-un fișier text
+void writeTreeInTextFile(Arbore arbore, const char *fileName) 
+{
+    FILE *file = fopen(fileName, "w");
+    if (file == NULL) 
+    {
+        printf("Eroare la deschiderea fisierului %s\n", fileName);
+        exit(EXIT_FAILURE);
+    }
+
+    // Scrierea datelor din arbore în fișierul text
+    writeNodeInTextFile(arbore, file);
+
+    fclose(file);
+}
+
+// Funcție auxiliară pentru scrierea nodurilor din arbore într-un fișier text
+void writeNodeInTextFile(Arbore arbore, FILE *file) 
+{
+    if (arbore != NULL) 
+    {
+        // Scrierea informațiilor despre student în fișierul text
+        fprintf(file, "Nume: %s, Varsta: %d, Nota: %.2f\n", arbore->student.nume, arbore->student.varsta, arbore->student.nota);
+        
+        // Parcurgerea subarborilor
+        writeNodeInTextFile(arbore->left, file);
+        writeNodeInTextFile(arbore->right, file);
+    }
+}
+
+
 // Funcție pentru eliberarea memoriei alocate pentru arbore (stergerea completa)
-void freeTree(Student *radacina) 
+void freeTree(Arbore radacina) 
 {
     if (radacina != NULL) 
     {
