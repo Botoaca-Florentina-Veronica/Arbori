b1a2741f4f4b3341a4b4faf32fda028184999c83
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-03-04 15:24:01 +0200

    Update main.c

--- bibliotecaStudenti/main.c
+++ bibliotecaStudenti/main.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "biblioteca.h"
-
 
 int main(void) 
 {
-    Student *radacina = NULL;
+    // Inițializare arbore binar ordonat
+    Arbore arbore = initializareArbore();
 
-    // Exemplu de adăugare a unor noduri
-    radacina = insertNode(radacina, "Alice", 9, 20);
-    radacina = insertNode(radacina, "Bob", 8, 22);
-    radacina = insertNode(radacina, "Charlie", 7, 21);
+    // Citirea datelor din fișierul binar într-un tablou de structuri
+    const char *fileName = "studenti.dat";
+    int nrInregistrari = 10; // presupunând că avem 10 înregistrări în fișier
+    Student studenti[1000]; // declararea tabloului pentru a stoca datele
+    readingFromBinaryFile(fileName, studenti, nrInregistrari);
 
-    printf("Parcurgere inordine:\n");
-    inOrderTraversal(radacina);
-
-    // Exemplu de ștergere a unui nod
-    radacina = deleteNode(radacina, "Bob");
-
-    printf("\nParcurgere inordine dupa stergere:\n");
-    inOrderTraversal(radacina);
-
-    // Exemplu de cautare a unui nod
-    char searchName[] = "Alice";
-    Student* foundNode = searchNode(radacina, searchName);
-    if (foundNode != NULL) 
+    // Adăugarea datelor în arbore
+    int i;
+    for (i = 0; i < nrInregistrari; i++) 
     {
-        printf("\n%s gasit: Nota: %d, Varsta: %d\n", searchName, foundNode->nota, foundNode->varsta);
-    } 
-    else 
-    {
-        printf("\n%s nu a fost gasit in arbore.\n", searchName);
+        addNode(&arbore, studenti[i]);
     }
 
-    // Eliberare memorie
-    freeTree(radacina);
+    // Citirea unei anumite înregistrări din fișier
+    int nrInregistrareCautata = 17; // de exemplu, citim înregistrarea cu numărul 17
+    Student studentCautat;
+    readRegistration(fileName, &studentCautat, nrInregistrareCautata);
+
+    // Afișarea arborelui
+    printf("Parcurgere inordine a arborelui:\n");
+    inOrderTraversal(arbore);
+
+    // Eliberarea memoriei alocate pentru arbore
+    freeTree(arbore);
     return 0;
 }
