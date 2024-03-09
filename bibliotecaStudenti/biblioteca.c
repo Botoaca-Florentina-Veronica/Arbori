10b1a4ff562d567308d193ac652cfdf81dc76c1c
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-03-09 20:52:21 +0200

    Update biblioteca.c

--- bibliotecaStudenti/biblioteca.c
+++ bibliotecaStudenti/biblioteca.c
@@ -1,216 +1,247 @@
 /*
-   Scrieti o biblioteca in care sa implementati operatiile generice pentru un arbore binar ordonat (ABO):
-Initializare, Adaugare nod, Suprimare Nod, Cautare Nod, Parcurgeri arbore.
-Tipul nodului considerati-l de tip struct. Se consideră că se citesc dintr-un fisier binar informatii cu privire la
-studenții de la facultate, fiecare având informații legate de nume(string), vârstă(int) și note(float). Acestea vor fi
-organizate sub forma unui arbore binar ordonat, care se foloseste si de functiile mentionate mai sus.
+Scrieti o biblioteca in C in care sa implementati urmatoarele:
+-creeaza un fisier binar cu 1000 inregistrari cu structura
+{
+   int h1;
+   float a1;
+}
+fisierul contine valori random!
+
+-functie de creeare si adaugare noduri pentru un arbore binar ordonat
+unde cheia este h1. Structura arbore binar:
+typedef struct arbore{
+   int cheie;  //h1
+   arbore *stanga;
+   arbore *dreapta;
+   int index;
+}
+unde index este indexul inregistrarii din fisier(incepe de la 0).
+
+-se citeste de la tastatura un un numar intreg, iar cu el se vor face
+2 cautari:
+	*cautam in fisierul binar, dupa h1 si afisam pe ecran valoarea lui a1,
+         si calculam numarul de comparratii
+	*cealalta cautare se realizeaza in arborele binar ordonat si se cauta dupa h1,
+         si se afiseaza valoarea a1 din fisierul binar, iar apoi se calculeaza 
+         numarul de comparatii
+cautarile se vor realiza folosind fseek+indice
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "biblioteca.h"
 
-
-Arbore initializareArbore() 
+// Funcție pentru generarea unei valori aleatoare între min și max
+int generare_aleatoare(int min, int max) 
 {
-    return NULL;
+    return min + rand() % (max - min + 1);
 }
 
-// Funcție pentru alocarea memoriei pentru un nou nod Student
-node *createNode(Student student) 
+// Funcție pentru crearea și popularea fișierului binar cu înregistrări random
+void creare_fisier_binar(const char *nume_fisier) 
 {
-    node *newNode;
-    newNode = (node *)malloc(sizeof(node));
-    if (newNode == NULL) 
+    FILE *file;
+    file = fopen(nume_fisier, "wb");
+    int i;
+    if (file == NULL) 
     {
-        printf("Eroare la alocarea memoriei pentru un nou nod.\n");
-        exit(1);
+        printf("Eroare la deschiderea fisierului.\n");
+        return;
     }
-    newNode->student = student;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+
+    srand(time(NULL));
+
+    Inregistrare inregistrare;
+    for (i = 0; i < 1000; i++) 
+    {
+        inregistrare.h1 = generare_aleatoare(1, 1000);
+        inregistrare.a1 = (float)generare_aleatoare(1, 1000) / 10;
+        fwrite(&inregistrare, sizeof(Inregistrare), 1, file);
+    }
+    fclose(file);
 }
 
-// Funcție pentru adăugarea unui nod în arbore(vom insera alfabetic)
-void addNode(Arbore *radacina, Student student) 
+// Funcție pentru crearea și adăugarea unui nod în arborele binar
+void adauga_nod_arbore(NodArbore **radacina, int cheie, int index) 
 {
     if (*radacina == NULL) 
     {
-        //daca rădăcina nu există, atunci putem adăuga noul nod, el devenind noua rădăcină
-        *radacina = createNode(student);
-    }
+        *radacina = (NodArbore *)malloc(sizeof(NodArbore));
+        (*radacina)->cheie = cheie;
+        (*radacina)->index = index;
+        (*radacina)->stanga = NULL;
+        (*radacina)->dreapta = NULL;
+    } 
     else 
     {
-        if (strcmp(student.nume, (*radacina)->student.nume) < 0) 
+        if (cheie < (*radacina)->cheie) 
         {
-            addNode(&(*radacina)->left, student);
+            adauga_nod_arbore(&((*radacina)->stanga), cheie, index);
         } 
         else 
         {
-            addNode(&(*radacina)->right, student);
+            adauga_nod_arbore(&((*radacina)->dreapta), cheie, index);
         }
     }
 }
 
-
-node *findMin(node *nod) 
+// Funcție pentru căutarea și afișarea valorii a1 din fisierul binar
+// bazată pe cheia h1, și calcularea numărului de comparații
+float cautare_fisier_binar(const char *nume_fisier, int cheie, int *numar_comparatii) 
 {
-    while (nod->left != NULL) 
+    FILE *f = fopen(nume_fisier, "rb");
+    if (f == NULL) 
     {
-        nod = nod->left;
+        printf("Eroare la deschiderea fisierului.\n");
+        return -1; // Valoare invalidă pentru a1
     }
-    return nod;
+
+    Inregistrare inregistrare;
+    *numar_comparatii = 0;
+
+    int index = 0;
+    while (fread(&inregistrare, sizeof(Inregistrare), 1, f)) 
+    {
+        (*numar_comparatii)++;
+        if (inregistrare.h1 == cheie) 
+        {
+            fclose(f);
+            FILE *f2 = fopen(nume_fisier, "rb");
+            if (f2 == NULL) 
+            {
+                printf("Eroare la deschiderea fisierului.\n");
+                return -1; // Valoare invalidă pentru a1
+            }
+            fseek(f2, index * sizeof(Inregistrare), SEEK_SET);
+            fread(&inregistrare, sizeof(Inregistrare), 1, f2);
+            fclose(f2);
+            return inregistrare.a1;
+        }
+        index++;
+    }
+    fclose(f);
+    return -1; // Valoare invalidă pentru a1
 }
 
-node *deleteNod(node *radacina, char *nume) 
+
+// Funcție pentru căutarea și afișarea valorii a1 din arborele binar ordonat
+// bazată pe cheia h1, și calcularea numărului de comparații
+float cautare_arbore(NodArbore *radacina, int cheie, int *numar_comparatii) 
+{
+    if (radacina == NULL) 
+    {
+        *numar_comparatii = 1;
+        return -1; // Valoare invalidă pentru a1
+    }
+
+    if (radacina->cheie == cheie) 
+    {
+        *numar_comparatii = 1;
+        return radacina->index;
+    } 
+    else if (cheie < radacina->cheie) 
+    {
+        (*numar_comparatii)++;
+        return cautare_arbore(radacina->stanga, cheie, numar_comparatii);
+    } 
+    else 
+    {
+        (*numar_comparatii)++;
+        return cautare_arbore(radacina->dreapta, cheie, numar_comparatii);
+    }
+}
+
+// Funcție pentru ștergerea unui nod din arbore
+NodArbore *sterge_nod_arbore(NodArbore *radacina, int cheie) 
 {
     if (radacina == NULL) 
     {
         return radacina;
     }
 
-    // Găsește nodul de șters în subarborele stâng sau drept
-    if (strcmp(nume, radacina->student.nume) < 0) 
+    if (cheie < radacina->cheie) 
     {
-        radacina->left = deleteNod(radacina->left, nume);
+        radacina->stanga = sterge_nod_arbore(radacina->stanga, cheie);
     } 
-    else if (strcmp(nume, radacina->student.nume) > 0) 
+    else if (cheie > radacina->cheie) 
     {
-        radacina->right = deleteNod(radacina->right, nume);
+        radacina->dreapta = sterge_nod_arbore(radacina->dreapta, cheie);
     } 
     else 
     {
-        // Nodul de șters are 0 sau 1 copil
-        if (radacina->left == NULL) 
+        // Nodul are unul sau niciun copil
+        if (radacina->stanga == NULL) 
         {
-            node *temp = radacina->right;
+            NodArbore *temp = radacina->dreapta;
             free(radacina);
             return temp;
-        }
-        else if (radacina->right == NULL) 
+        } 
+        else if (radacina->dreapta == NULL) 
         {
-            node *temp = radacina->left;
+            NodArbore *temp = radacina->stanga;
             free(radacina);
             return temp;
         }
 
-        // Nodul de șters are 2 copii
-        node *temp = findMin(radacina->right);
-        radacina->student = temp->student;
-        radacina->right = deleteNod(radacina->right, temp->student.nume);
+        // Nodul are doi copii: găsește succesorul înordine (cel mai mic nod din subarborele drept)
+        NodArbore *succesor = radacina->dreapta;
+        while (succesor && succesor->stanga != NULL) 
+        {
+            succesor = succesor->stanga;
+        }
+
+        // Copiază cheia și indexul succesorului în nodul curent
+        radacina->cheie = succesor->cheie;
+        radacina->index = succesor->index;
+
+        // Șterge succesorul
+        radacina->dreapta = sterge_nod_arbore(radacina->dreapta, succesor->cheie);
     }
     return radacina;
 }
 
-
-// Funcție pentru afișarea arborelui în inordine (stanga, radacina, dreapta)
-void inOrderTraversal(Arbore radacina) 
+// Funcție pentru parcurgerea arborelui în preordine
+void preordine(NodArbore *radacina) 
 {
     if (radacina != NULL) 
     {
-        inOrderTraversal(radacina->left);
-        printf("Nume: %s, Varsta: %d, Nota: %.2f\n", radacina->student.nume, radacina->student.varsta, radacina->student.nota);
-        inOrderTraversal(radacina->right);
+        printf("%d ", radacina->cheie);
+        preordine(radacina->stanga);
+        preordine(radacina->dreapta);
     }
 }
 
-
-// Funcție pentru afișarea arborelui în preordine (radacina, stanga, dreapta)
-void preOrderTraversal(Arbore radacina)
+// Funcție pentru parcurgerea arborelui în inordine
+void inordine(NodArbore *radacina) 
 {
     if (radacina != NULL) 
     {
-        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->student.nume, radacina->student.nota, radacina->student.varsta);
-        preOrderTraversal(radacina->left);
-        preOrderTraversal(radacina->right);
+        inordine(radacina->stanga);
+        printf("%d ", radacina->cheie);
+        inordine(radacina->dreapta);
     }
 }
 
-
-// Funcție pentru afișarea arborelui în postordine (stanga, dreapta, radacina)
-void postOrderTraversal(Arbore radacina) 
+// Funcție pentru parcurgerea arborelui în postordine
+void postordine(NodArbore *radacina) 
 {
     if (radacina != NULL) 
     {
-        postOrderTraversal(radacina->left);
-        postOrderTraversal(radacina->right);
-        printf("Nume: %s, Nota: %d, Varsta: %d\n", radacina->student.nume, radacina->student.nota, radacina->student.varsta);
+        postordine(radacina->stanga);
+        postordine(radacina->dreapta);
+        printf("%d ", radacina->cheie);
     }
 }
-
-// Funcție pentru citirea datelor dintr-un fișier binar într-un tablou de structuri
-void readingFromBinaryFile(const char *fileName, Student *studenti, int nrInregistrari) 
-{
-    FILE *file = fopen(fileName, "rb");
-    if (file == NULL) 
-    {
-        printf("Eroare la deschiderea fisierului %s\n", fileName);
-        exit(EXIT_FAILURE);
-    }
-
-    // Citirea datelor pentru fiecare înregistrare
-    fread(studenti, sizeof(Student), nrInregistrari, file);
-    fclose(file);
-}
-
-// Funcție pentru citirea unei anumite înregistrări identificate prin numărul său din fișier
-void readRegistration(const char *fileName, Student *student, int nrInregistrare) 
-{
-    FILE *file = fopen(fileName, "rb");
-    if (file == NULL) 
-    {
-        printf("Eroare la deschiderea fisierului %s\n", fileName);
-        exit(EXIT_FAILURE);
-    }
-
-    // Folosirea fseek pentru a poziționa cursorul la începutul înregistrării dorite
-    fseek(file, (nrInregistrare - 1) * sizeof(Student), SEEK_SET);
-
-    // Citirea înregistrării
-    fread(student, sizeof(Student), 1, file);
-    fclose(file);
-}
-
-// Funcție pentru scrierea datelor dintr-un arbore într-un fișier text
-void writeTreeInTextFile(Arbore arbore, const char *fileName) 
-{
-    FILE *file = fopen(fileName, "w");
-    if (file == NULL) 
-    {
-        printf("Eroare la deschiderea fisierului %s\n", fileName);
-        exit(EXIT_FAILURE);
-    }
-
-    // Scrierea datelor din arbore în fișierul text
-    writeNodeInTextFile(arbore, file);
-
-    fclose(file);
-}
-
-// Funcție auxiliară pentru scrierea nodurilor din arbore într-un fișier text
-void writeNodeInTextFile(Arbore arbore, FILE *file) 
-{
-    if (arbore != NULL) 
-    {
-        // Scrierea informațiilor despre student în fișierul text
-        fprintf(file, "Nume: %s, Varsta: %d, Nota: %.2f\n", arbore->student.nume, arbore->student.varsta, arbore->student.nota);
-        
-        // Parcurgerea subarborilor
-        writeNodeInTextFile(arbore->left, file);
-        writeNodeInTextFile(arbore->right, file);
-    }
-}
-
 
 // Funcție pentru eliberarea memoriei alocate pentru arbore (stergerea completa)
-void freeTree(Arbore radacina) 
+void freeTree(NodArbore *radacina) 
 {
     if (radacina != NULL) 
     {
-        freeTree(radacina->left);
-        freeTree(radacina->right);
+        freeTree(radacina->stanga);
+        freeTree(radacina->dreapta);
         free(radacina);
     }
 }
