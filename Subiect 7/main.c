5d04bcda3dc8919693714a854e540c77fbe827ae
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-06-04 20:34:05 +0300

    Update main.c

--- Subiect 7/main.c
+++ Subiect 7/main.c
@@ -1,5 +1,5 @@
 /*
-  Se da un gra neorientat ponderat conex, cu n varfuri si m muchii-in care fiecare muchie are asociat un cost, numar natural
+  Se da un graf neorientat ponderat conex, cu n varfuri si m muchii-in care fiecare muchie are asociat un cost, numar natural
 strict pozitiv
   Folosind algoritmul lui Prim, determinati un arbore partial de cost minim
 */
@@ -10,103 +10,136 @@ strict pozitiv
 #include <limits.h>
 
 #define MAX_VERTICES 100
-#define INF 999999
 
 // Structura pentru reprezentarea muchiilor
-struct Edge {
-    int source, destination, weight;
-};
+typedef struct Edge {
+    int source;
+    int destination;
+    int weight;
+} Edge;
 
 // Structura pentru reprezentarea grafului
-struct Graph {
-    int numVertices, numEdges;
-    struct Edge* edge;
-};
+typedef struct Graph {
+    int numVertices;
+    int numEdges;
+    Edge *edges;
+} Graph;
 
 // Functie pentru initializarea grafului
-struct Graph* createGraph(int numVertices, int numEdges) 
+Graph* createGraph(int numVertices, int numEdges) 
 {
-    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    Graph *graph = (Graph *) malloc(sizeof(Graph));
     graph->numVertices = numVertices;
     graph->numEdges = numEdges;
-    graph->edge = (struct Edge*) malloc(graph->numEdges * sizeof(struct Edge));
+    graph->edges = (Edge *) malloc(numEdges * sizeof(Edge));
     return graph;
 }
 
 // Functie pentru adaugarea unei muchii in graf
-void addEdge(struct Graph* graph, int source, int destination, int weight) 
+void addEdge(Graph *graph, int edgeIndex, int source, int destination, int weight) 
 {
-    graph->edge[source].source = source;
-    graph->edge[source].destination = destination;
-    graph->edge[source].weight = weight;
+    graph->edges[edgeIndex].source = source;
+    graph->edges[edgeIndex].destination = destination;
+    graph->edges[edgeIndex].weight = weight;
 }
 
 // Functie pentru a gasi varful cu cel mai mic cost
 int minKey(int key[], bool mstSet[], int numVertices) 
 {
+    int v;
     int min = INT_MAX, min_index;
-    for (int v = 0; v < numVertices; v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
+    for (v = 0; v < numVertices; v++) 
+    {
+        if (mstSet[v] == false && key[v] < min) 
+        {
+            min = key[v];
+            min_index = v;
+        }
+    }
     return min_index;
 }
 
 // Functie pentru a afisa arborele partial de cost minim
-void printMST(int parent[], struct Edge graph[], int numVertices) 
+void printMST(int parent[], Edge edges[], int numVertices) 
 {
+    int i;
     printf("Muchiile arborelui partial de cost minim:\n");
-    for (int i = 1; i < numVertices; i++)
+    for (i = 1; i < numVertices; i++) 
+    {
         printf("%d - %d\n", parent[i], i);
+    }
 }
 
 // Functie pentru a calcula si a afisa arborele partial de cost minim folosind algoritmul lui Prim
-void primMST(struct Graph* graph) 
+void primMST(Graph *graph) 
 {
-    int parent[MAX_VERTICES]; // Array pentru a stoca arborele partial de cost minim
-    int key[MAX_VERTICES]; // Array pentru a stoca costul minim pentru fiecare varf
-    bool mstSet[MAX_VERTICES]; // Array pentru a marca varfurile incluse in arborele partial de cost minim
-    
-    // Initializare
-    for (int i = 0; i < graph->numVertices; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
-    key[0] = 0; // Primul varf este intotdeauna radacina arborelui
-    
-    parent[0] = -1; // Niciun varf nu are parinte in arborele partial de cost minim
-    
-    // Construirea arborelui partial de cost minim
-    for (int count = 0; count < graph->numVertices - 1; count++) {
-        int u = minKey(key, mstSet, graph->numVertices); // Alegerea varfului cu cel mai mic cost nevizitat
-        mstSet[u] = true; // Marcharea varfului ca vizitat
-        
-        // Actualizarea costului minim pentru varfurile adiacente varfului selectat recent
-        for (int v = 0; v < graph->numVertices; v++)
-            if (graph->edge[u].weight && mstSet[v] == false && graph->edge[u].weight < key[v])
-                parent[v] = u, key[v] = graph->edge[u].weight;
+    int i, u, e, v;
+    int numVertices = graph->numVertices;
+    int parent[MAX_VERTICES];
+    int key[MAX_VERTICES];
+    bool mstSet[MAX_VERTICES];
+
+    for (i = 0; i < numVertices; i++) 
+    {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
     }
-    
-    // Afisarea arborelui partial de cost minim
-    printMST(parent, graph->edge, graph->numVertices);
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < numVertices - 1; count++) 
+    {
+        u = minKey(key, mstSet, numVertices);
+        mstSet[u] = true;
+
+        for (v = 0; v < numVertices; v++) 
+        {
+            for (e = 0; e < graph->numEdges; e++) 
+            {
+                Edge edge = graph->edges[e];
+                if (edge.source == u && !mstSet[edge.destination] && edge.weight < key[edge.destination]) 
+                {
+                    parent[edge.destination] = u;
+                    key[edge.destination] = edge.weight;
+                } 
+                else if (edge.destination == u && !mstSet[edge.source] && edge.weight < key[edge.source]) 
+                {
+                    parent[edge.source] = u;
+                    key[edge.source] = edge.weight;
+                }
+            }
+        }
+    }
+    printMST(parent, graph->edges, numVertices);
 }
 
 // Functia principala
-int main(void)
+int main(void) 
 {
     int i;
-    int numVertices, numEdges;
-    printf("Introduceti numarul de varfuri si muchii: ");
-    scanf("%d %d", &numVertices, &numEdges);
+    int numVertices = 5;
+    int numEdges = 7;
 
-    struct Graph* graph = createGraph(numVertices, numEdges);
+    Graph *graph = createGraph(numVertices, numEdges);
 
-    printf("Introduceti muchiile si costurile asociate:\n");
-    for (i = 0; i < numEdges; ++i) 
+    int edges[][3] = 
     {
-        int source, destination, weight;
-        scanf("%d %d %d", &source, &destination, &weight);
-        addEdge(graph, source, destination, weight);
+        {0, 1, 5},
+        {1, 4, 10},
+        {1, 2, 2},
+        {2, 4, 3},
+        {2, 3, 9},
+        {3, 4, 11},
+        {4, 0, 7}
+    };
+
+    for (i = 0; i < numEdges; i++) 
+    {
+        addEdge(graph, i, edges[i][0], edges[i][1], edges[i][2]);
     }
 
     primMST(graph);
-
+    free(graph->edges);
+    free(graph);
     return 0;
 }
