0ac7d58723de569f13506b850e97d0aed4ddaed7
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-06-21 21:01:35 +0300

    Update main.c

--- Subiect 8/main.c
+++ Subiect 8/main.c
@@ -1,103 +1,342 @@
-/*  Se da harta unei regiuni, reprezentata prin matricea de adiacenta a unui graf neorientat, ponderat, nodurile reprezentand orasele din regiune
-si ponderile reprezentand distantele dintre orase 
-    Sa se scrie un program care determina lungimea minima necesara de teava care trebuie folosita de o companie de apa care conecteaza toate orasele.
+/*
+  Se da un arbore AVL de studenti in care sunt stocate datele acestuia (marca: char,
+anul nasterii: int, admin/respins: boolean). Sa se stocheze doar studentii admisi
+intr-un arbore de regasire si sa se calculeze inaltimea celor 2 arbori.
 */
 
-//vom folosi algoritmul lui Prim
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#include <limits.h>
+#include <string.h>
 
-#define MAX 100
-
-// Funcția pentru a găsi vârful cu valoarea cheie minimă din setul de vârfuri care nu sunt incluse în MST
-// (MST- minimal spanning tree)
-int minKey(int key[], bool mstSet[], int n) 
+// Structura pentru un nod din arborele AVL
+typedef struct TNodAVL
 {
-    int v;
-    int min = INT_MAX;
-    int min_index;
+    int cheie;
+    int ech;
+    struct TNodAVL *stg;
+    struct TNodAVL *dr;
+} TNodAVL;
 
-    for (v = 0; v < n; v++)
+// Structura pentru un student
+typedef struct
+{
+    char marca[12];
+    int anul_nasterii;
+    bool admis;
+} Student;
+
+// Structura pentru un nod din arborele de regăsire
+typedef struct NodArboreDeRegasire
+{
+    struct NodArboreDeRegasire *alfabet[27];
+} NodArboreDeRegasire;
+
+#define TRUE 1
+#define FALSE 0
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+// Funcția pentru inițializarea unui nod din arborele de regăsire
+void Initializare(NodArboreDeRegasire *Nod)
+{
+    char c;
+    for (c = 'A'; c <= '['; c++)
+        Nod->alfabet[c - 'A'] = NULL;
+}
+
+// Funcția pentru atribuirea unui nod din arborele de regăsire
+void Atribuie(NodArboreDeRegasire *Nod, char c, NodArboreDeRegasire *p)
+{
+    Nod->alfabet[c - 'A'] = p;
+}
+
+// Funcția pentru obținerea valorii unui nod din arborele de regăsire
+NodArboreDeRegasire *Valoare(NodArboreDeRegasire *Nod, char c)
+{
+    return Nod->alfabet[c - 'A'];
+}
+
+// Funcția pentru crearea unui nod nou în arborele de regăsire
+void NodNou(NodArboreDeRegasire *Nod, char c)
+{
+    Nod->alfabet[c - 'A'] = (NodArboreDeRegasire *)malloc(sizeof(NodArboreDeRegasire));
+    Initializare(Nod->alfabet[c - 'A']);
+}
+
+// Funcția pentru adăugarea unui cuvânt în arborele de regăsire
+void Adauga(char *x, NodArboreDeRegasire *cuvinte)
+{
+    unsigned i;
+    NodArboreDeRegasire *t;
+    t = cuvinte;
+    for (i = 0; i < strlen(x); i++)
     {
-        if (mstSet[v] == false && key[v] < min)
+        if (Valoare(t, x[i]) == NULL)
+            NodNou(t, x[i]);
+        t = Valoare(t, x[i]);
+    }
+    Atribuie(t, '[', t);
+}
+
+// Funcția pentru calculul înălțimii arborelui de regăsire
+void CalculInaltime(NodArboreDeRegasire *Nod, int *inaltime_max, int nivel)
+{
+    bool isLeaf = true;
+    for (char c = 'A'; c <= 'Z'; c++)
+    {
+        if (Valoare(Nod, c))
         {
-            min = key[v];
-            min_index = v;
+            isLeaf = false;
+            CalculInaltime(Valoare(Nod, c), inaltime_max, nivel + 1);
+        }
+    }
+    if (isLeaf && Valoare(Nod, '['))
+    {
+        if (nivel > *inaltime_max)
+        {
+            *inaltime_max = nivel;
+        }
+    }
+}
+
+// Funcția pentru a distruge un arbore AVL
+void DistrugeAVL(TNodAVL *r)
+{
+    if (!r)
+        return;
+    DistrugeAVL(r->stg);
+    DistrugeAVL(r->dr);
+    free(r);
+}
+
+// Funcția insertEchilibrat de pe cv
+TNodAVL *InsertEchilibrat(int x, TNodAVL *p, int *h)
+{
+    TNodAVL *p1, *p2;
+
+    if (!p) // cheia nu e in arbore; se insereaza
+    {
+        p = malloc(sizeof(TNodAVL));
+        p->cheie = x;
+        p->ech = 0;            // nodul nou inserat creaza un subarbore unitar, care este mereu echilibrat
+        p->stg = p->dr = NULL; // noul nod este frunza
+        *h = TRUE;
+        return p;
+    }
+
+    if (x < p->cheie)
+    {
+        // daca cheia de inserat < cheia curenta,
+        // se insereaza in subarborele stang
+
+        /*  NOTAȚIE
+            ech = 0: Nodul este perfect echilibrat.
+            ech = 1: Subarborele drept este cu o unitate mai înalt decât subarborele stâng.
+            ech = -1: Subarborele stâng este cu o unitate mai înalt decât subarborele drept.
+        */
+        p->stg = InsertEchilibrat(x, p->stg, h);
+        if (*h == TRUE) // ramura stanga a crescut in inaltime
+        {
+            switch (p->ech)
+            {
+            case 1:         // subarborele era dezechilibrat in dreapta
+                p->ech = 0; // adaugare in subarborele stang => echilibru
+                *h = FALSE; // deci h rămâne fals, arborele nu crește în înălțime
+                break;
+            case 0:          // subarborele era in echilibru, iar prin inserarea nodului în subarborele stâng, provocăm dezechilibru în stânga
+                p->ech = -1; // dezechilibrat cu un nivel in stanga
+                break;
+            case -1:               // subarborele era dezechilibrat in stanga cu un nivel,
+                                   // acum e cu doua nivele => necesită reechilibrare
+                p1 = p->stg;       // aici alegem p1 ca fiind subarborele ce conține cele 3 noduri pe care le vom roti
+                if (p1->ech == -1) // cazul 1 stanga(LL), dezechilibru la fiu in stanga
+                {
+                    /*Se efectueaza o rotatie la dreapta a subarborelui p1, astfel:
+                       - fiul drept al nodului p1 este "decuplat" de arbore;
+                       - nodul p devine fiul drept al nodului p1;
+                       - nodul "decuplat" devine fiul stang al nodului p.
+                    */
+                    p->stg = p1->dr;
+                    p1->dr = p;
+                    p->ech = 0;
+                    p = p1;
+                }
+                else // cazul 2 stanga(LR), dezechilibru la fiu in dreapta
+                {
+                    /*Se efectueaza o rotatie la stanga a subarborelui p2, astfel:
+                       - fiul stanga al nodului p2 este "decuplat" de arbore;
+                       - nodul p2->stg devine fiul drept al nodului p1;
+                       - nodul p1 devine fiul stang al nodului "decuplat";
+                       - nodul "decuplat" p2 devine fiul stang al nodului p.
+
+                       Se efectueaza o rotatie la dreapta a subarborelui p2, astfel:
+                       - fiul stanga al nodului p (p2) este "decuplat" de arbore;
+                       - nodul p2->dr devine fiul stang al nodului p;
+                       - nodul p devine fiul drept al nodului "decuplat" p2.
+                    */
+                    p2 = p1->dr;
+                    p1->dr = p2->stg;
+                    p2->stg = p1;
+                    p->stg = p2->dr;
+                    p2->dr = p;
+
+                    if (p2->ech == -1)
+                        p->ech = +1;
+                    else
+                        p->ech = 0;
+
+                    if (p2->ech == +1)
+                        p1->ech = -1;
+                    else
+                        p1->ech = 0;
+
+                    p = p2;
+                } // caz 2 stanga
+                p->ech = 0;
+                *h = FALSE; // nu s-a schimbat diferenta de nivel
+                break;
+            } // switch
+        }
+    }
+    else if (x > p->cheie)
+    {
+        p->dr = InsertEchilibrat(x, (TNodAVL *)p->dr, h);
+        if (*h) // ramura dreapta a crescut in inaltime
+            switch (p->ech)
+            {
+            case -1:        // subarborele era dezechilibrat in stanga
+                p->ech = 0; // adaugare in subarborele drept => echilibru
+                *h = FALSE;
+                break;
+            case 0:          // subarborele era in echilibru
+                p->ech = +1; // dezechilibrat cu un nivel in stanga
+                break;
+            case +1:               // subarborele era dezechilibrat in dreapta cu un nivel,
+                                   // acum e cu doua nivele => reechilibrare
+                p1 = p->dr;        // la fel ca mai sus, în p1 stocăm acum subarborele drept din arborele nostru, care constă în 3 noduri ce necesită reechilibrare
+                if (p1->ech == +1) // cazul 1 dreapta(RR)
+                {
+                    /*Se efectueaza o rotatie la stanga a subarborelui p1, astfel:
+                       - nodul p1->stg este "decuplat" de arbore;
+                       - nodul p devine fiul stang al nodului p1;
+                       - nodul "decuplat" devine fiul drept al nodului p.
+                    */
+                    p->dr = p1->stg;
+                    p1->stg = p;
+                    p->ech = 0;
+                    p = p1;
+                }
+                else // cazul 2 dreapta(RL)
+                {
+                    /*Se efectueaza o rotatie la dreapta a subarborelui p2, astfel:
+                       - nodul p2->dr este "decuplat" de arbore;
+                       - nodul p1 devine fiul drept al nodului p2;
+                       - nodul p2 devine fiul drept al nodului p
+                       - nodul "decuplat" devine fiul stang al nodului p1.
+
+                      Se efectueaza o rotatie la stanga a subarborelui p2, astfel:
+                       - nodul p2->stg este "decuplat" de arbore;
+                       - nodul p devine fiul stang al nodului p2;
+                       - nodul "decuplat" devine fiul drept al nodului p.
+                    */
+                    p2 = p1->stg;
+                    p1->stg = p2->dr;
+                    p2->dr = p1;
+                    p->dr = p2->stg;
+                    p2->stg = p;
+
+                    if (p2->ech == 1)
+                        p->ech = -1;
+                    else
+                        p->ech = 0;
+
+                    if (p2->ech == -1)
+                        p1->ech = +1;
+                    else
+                        p1->ech = 0;
+
+                    p = p2;
+                } // caz 2 dreapta
+                p->ech = 0;
+                *h = FALSE; // înălțimea nu se schimbă
+            } // switch
+    }
+    return p;
+}
+
+// Funcția pentru adăugarea unui nod în arborele AVL
+TNodAVL *AdaugaAVL(TNodAVL *r, int k)
+{
+    int h = FALSE;
+    return InsertEchilibrat(k, r, &h);
+}
+
+// Funcția pentru calculul înălțimii unui arbore AVL
+int InaltimeAVL(TNodAVL *r)
+{
+    if (!r)
+        return 0;
+    return (MAX(InaltimeAVL(r->stg), InaltimeAVL(r->dr))) + 1;
+}
+
+int main(void)
+{
+    // Definirea și inițializarea studenților
+    int i;
+    Student studenti[] =
+        {
+            {"A1", 1995, true},
+            {"A2", 1996, false},
+            {"A3", 1994, true},
+            {"A4", 1993, false},
+            {"A5", 1992, true}};
+
+    // Calculul numărului de studenți
+    int nrStudenti = sizeof(studenti) / sizeof(studenti[0]);
+
+    // Inițializarea arborilor AVL
+    TNodAVL *arboreStudenti = NULL;
+    NodArboreDeRegasire *arboreRegasire = (NodArboreDeRegasire *)malloc(sizeof(NodArboreDeRegasire));
+    Initializare(arboreRegasire);
+
+    // Adăugarea tuturor studenților în arborele AVL și arborele de regăsire
+    for (i = 0; i < nrStudenti; i++)
+    {
+        arboreStudenti = AdaugaAVL(arboreStudenti, studenti[i].anul_nasterii);
+        if (studenti[i].admis)
+        {
+            Adauga(studenti[i].marca, arboreRegasire);
         }
     }
 
-    return min_index;
-}
+    // Afișarea înălțimii arborelui AVL
+    printf("Inaltimea arborelui AVL: %d\n", InaltimeAVL(arboreStudenti));
 
-// Funcția pentru a construi și afișa MST utilizând matricea de adiacență reprezentată ca graful
-int primMST(int graph[MAX][MAX], int n) 
-{
-    int i, u, v;
-    int parent[MAX]; // Array pentru a stoca MST-ul construit
-    int key[MAX]; // Valori cheie utilizate pentru a alege vârfurile minime
-    bool mstSet[MAX]; // Pentru a reprezenta setul de vârfuri incluse în MST
+    // Calculul și afișarea înălțimii arborelui de regăsire
+    int inaltime_max = 0;
+    CalculInaltime(arboreRegasire, &inaltime_max, 0);
+    printf("Inaltimea arborelui de regasire: %d\n", inaltime_max);
 
-    // Inițializăm toate cheile ca INFINIT
-    for (i = 0; i < n; i++)
-    {
-        key[i] = INT_MAX;
-        mstSet[i] = false;
-    }
-
-    // Întotdeauna includem primul vârf în MST
-    key[0] = 0; // Facem valoarea cheie a acestui vârf 0 pentru a-l alege primul
-    parent[0] = -1; // Primul nod este întotdeauna rădăcina MST-ului
-
-    // MST-ul va avea n vârfuri
-    for (int count = 0; count < n - 1; count++) 
-    {
-        u = minKey(key, mstSet, n);
-        mstSet[u] = true;
-        //inca nu am pus drumul gasit in u in graful minim de acoperire, doar am cautat si gasit cel mai scurt drum(indexul din matricea de adiacenta)
-        // si l-am retinut in u
-        //urmeaza sa introduc acest drum in arborele minim aici:
-
-        // Actualizați valoarea cheii și indexul părintelui vârfurilor adiacente vârfului ales
-        for (v = 0; v < n; v++)
-
-            // graph[u][v] este diferit de 0 doar pentru vârfurile adiacente
-            // mstSet[v] este fals pentru vârfurile încă neincluse în MST
-            // Actualizați cheia doar dacă graph[u][v] este mai mică decât key[v]
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-            {
-                parent[v] = u;
-                key[v] = graph[u][v];
-            }
-    }
-
-    // Calculăm suma totală a MST-ului
-    int total_weight = 0;
-    for (i = 1; i < n; i++)
-    {
-        total_weight += graph[i][parent[i]];
-    }
-    return total_weight;
-}
-
-int main(void) 
-{
-    // Inițializăm numărul de orașe
-    int n = 5;
-
-    // Inițializăm matricea de adiacență
-    int graph[MAX][MAX] = 
-    {
-        {0, 2, 0, 6, 0},
-        {2, 0, 3, 8, 5},
-        {0, 3, 0, 0, 7},
-        {6, 8, 0, 0, 9},
-        {0, 5, 7, 9, 0},
-    };
-
-    // Calculăm și afișăm lungimea totală a țeavii necesare
-    int total_length = primMST(graph, n);
-    printf("Lungimea minima de teava necesara pentru a conecta toate orasele este: %d\n", total_length);
+    // Eliberarea memoriei pentru arborele AVL
+    DistrugeAVL(arboreStudenti);
 
     return 0;
 }
+
+/*
+Arbore AVL:
+         1995
+        /    \
+    1993      1996
+   /   \
+1992   1994
+
+
+Arbore de regasire:
+(root)
+  └─ '199'
+      ├─ '5'
+      ├─ '4'
+      └─ '2'
+
+*/
