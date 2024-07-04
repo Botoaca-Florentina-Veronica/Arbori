047216c49a5a7339e24a3008df545437697aeab2
Vera <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-07-04 20:29:11 +0300

    Update main.c

--- Arbori-de-regăsire/main.c
+++ Arbori-de-regăsire/main.c
@@ -2,52 +2,66 @@
 #include <stdio.h>
 #include <string.h>
 
-// Definim structura NodArboreDeRegasire
-typedef struct NodArboreDeRegasire
+typedef struct Nod
 {
-    struct NodArboreDeRegasire *alfabet[27]; // 26 litere + 1 pentru '[', deci 27
-} NodArboreDeRegasire;
+    struct Nod *alfabet[27]; // 26 litere + 1 pentru '[', deci 27
+} Nod;
 
-void Initializare(NodArboreDeRegasire *Nod)
+void Initializare(Nod *Nod)
 {
     // ATENȚIE, aceasta funcție trebuie apelată cu *Nod deja alocat
     char c;
     for (c = 'A'; c <= '['; c++)
+    {
+        // imi iterez alfabetic prin toate cele 27 de caractere, de la 0 la 26
+        // și mi le inițializez cu NULL, fiind de tip char, adică pointer
+        // indicând că nu există niciun copil (subarbore) pentru acel caracter
         Nod->alfabet[c - 'A'] = NULL;
+    }
 }
 
-void Atribuie(NodArboreDeRegasire *Nod, char c, NodArboreDeRegasire *p)
+void Atribuie(Nod *nod, char c, Nod *p)
 {
-    Nod->alfabet[c - 'A'] = p;
+    nod->alfabet[c - 'A'] = p;
 }
 
-NodArboreDeRegasire *Valoare(NodArboreDeRegasire *Nod, char c)
+// Această funcție returnează pointerul către nodul asociat cu caracterul c din nodul curent
+// Practic, verifică dacă există un subarbore pentru acel caracter și returnează adresa acelui subarbore
+Nod *Valoare(Nod *Nod, char c)
 {
     return Nod->alfabet[c - 'A'];
 }
 
-void NodNou(NodArboreDeRegasire *Nod, char c)
+void NodNou(Nod *nod, char c)
 {
-    Nod->alfabet[c - 'A'] = (NodArboreDeRegasire *)malloc(sizeof(NodArboreDeRegasire));
-    Initializare(Nod->alfabet[c - 'A']);
+    nod->alfabet[c - 'A'] = (Nod *)malloc(sizeof(Nod));
+    Initializare(nod->alfabet[c - 'A']);
 }
 
-void Adauga(char *x, NodArboreDeRegasire *cuvinte) // x = cuvântul, cuvinte = rădăcina arborelui de regăsire
+// Această funcție adaugă un cuvânt în arborele de regăsire. Parcurge fiecare caracter al cuvântului x,
+// creând noduri noi dacă acestea nu există și avansează în arbore până la sfârșitul cuvântului
+// La sfârșit, adaugă un terminator ('[') pentru a marca sfârșitul cuvântului
+void Adauga(char *x, Nod *cuvinte) // x = cuvântul, cuvinte = rădăcina arborelui de regăsire
 {
     unsigned i;
-    NodArboreDeRegasire *t;
+    Nod *t;
     t = cuvinte;
 
     for (i = 0; i < strlen(x); i++)
     {
         if (Valoare(t, x[i]) == NULL) // nodul curent nu are fiu pentru caracterul x[i], deci se crează unul nou
+        {
             NodNou(t, x[i]);
+        }
         t = Valoare(t, x[i]); // avansez în arborele de regăsire
     }
     Atribuie(t, '[', t); // se face o buclă pentru '[', pentru a marca un nod terminal
 }
 
-void Parcurge(NodArboreDeRegasire *Nod, char cuv[], int niv) // nodul curent, un buffer pentru construirea cuvântului, nivelul curent
+// Această funcție parcurge arborele și afișează toate cuvintele stocate
+// Utilizând un buffer cuv și un nivel niv, funcția adaugă litere în buffer și avansează recursiv în subarbori
+// Dacă găsește un terminator ('['), marchează sfârșitul unui cuvânt și îl afișează
+void Parcurge(Nod *Nod, char cuv[], int niv) // nodul curent, un buffer pentru construirea cuvântului, nivelul curent
 {
     char c;
     for (c = 'A'; c <= 'Z'; c++) // pentru toate literele considerate
@@ -67,7 +81,7 @@ void Parcurge(NodArboreDeRegasire *Nod, char cuv[], int niv) // nodul curent, un
 
 int main(void)
 {
-    NodArboreDeRegasire *radacina = (NodArboreDeRegasire *)malloc(sizeof(NodArboreDeRegasire));
+    Nod *radacina = (Nod *)malloc(sizeof(Nod));
     Initializare(radacina);
 
     // Adăugăm cuvinte în arbore
